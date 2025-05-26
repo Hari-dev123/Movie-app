8a5b09f6d9365866d5706ec5de5c4235b8cb98a4
HARIPRASATH G <140241512+Hari-dev123@users.noreply.github.com>
2025-05-26 17:47:43 +0530

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -22,7 +22,7 @@ const Footer = () => {
                     <li className="menuItem">FAQ</li>
                 </ul>
                 <div className="infoText">
-                Welcome to Movix, your premier destination for a cinematic journey at your fingertips. Dive into an extensive library, enjoy seamless streaming, and explore a world of entertainment without borders. Movix offers personalized recommendations in a user-friendly interface, making every click a gateway to a tailored cinematic experience.
+                Welcome to ShowTime, your premier destination for a cinematic journey at your fingertips. Dive into an extensive library, enjoy seamless streaming, and explore a world of entertainment without borders. ShowTime offers personalized recommendations in a user-friendly interface, making every click a gateway to a tailored cinematic experience.
                 </div>
                 <div className="socialIcons">
                     <span className="icon">
